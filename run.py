a5e6d2e91476a4331335723194c0af1145f1f9ca
Fanky <106883384+fanky86@users.noreply.github.com>
2026-05-10 01:18:17 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,7 @@
+# run.py
+from main import *
+
+if __name__ == '__main__':
+    # Panggil main yang sudah diatur
+    # Karena main.py memiliki if __name__ == '__main__', kita bisa langsung import dan jalankan
+    pass
